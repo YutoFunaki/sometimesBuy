fd9e487ae6c991d488870603f71608fe584455a6
YutoFunaki <104195830+YutoFunaki@users.noreply.github.com>
2022-10-10 04:05:28 +0900

    Initial Commit

---
+++ sometimesBuy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sometimesBuy
+//
+//  Created by 船木勇斗 on 2022/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
